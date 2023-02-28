55406c8c8e024d329972212b1166857984a29b3f
sari1995 <sarikapanhalkar276@gmail.com>
2023-02-28 17:29:09 +0530

    commit

--- src/test/java/demogit/Test.java
+++ src/test/java/demogit/Test.java
@@ -2,6 +2,11 @@ package demogit;
 
 public class Test {
 
+	public void name() {
+		System.out.println(20+2);
+		System.out.println("output");
+		
+	}
 	public static void main(String[] args) {
 
 		System.out.println("Hii");
