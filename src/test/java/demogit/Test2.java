69749b51d5d5efadea27e4da87f5eb18aacc0cd2
sari1995 <sarikapanhalkar276@gmail.com>
2023-03-02 14:25:54 +0530

    second commit

--- src/test/java/demogit/Test2.java
+++ src/test/java/demogit/Test2.java
@@ -2,6 +2,9 @@ package demogit;
 
 public class Test2 {
 
+	public  void abc() {
+		System.out.println(" I Am Sarika");
+	}
 	public static void main(String[] args) {
       System.out.println("Rohit Patil");
       System.out.println("Class");
